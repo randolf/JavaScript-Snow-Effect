734d689e17757deea4a49ea36a0fe62a2d754d63
Randolf Richardson <randolf@richardson.tw>
2021-03-01 09:43:32 -0800

    Added horizontal snow particle movement code to snoweffects.js
    This code adds a subtle horizontal movement characteristic to the snow particles to give the snowfall a slightly more natural motion overall.  (The horizontal movement parameters can be fine-tuned by altering the "horizontalMovementFrequency" and "horizontalMovementSize" variables.)

--- snoweffect.js
+++ snoweffect.js
@@ -1,16 +1,19 @@
 // -------------------------------------------------------------------------------------------------
 // JavaScript Snow Effect - © Copyright 2020 - Jam-Es.com
+// Horizontal snow particle movement code contributed by Randolf Richardson on 2021-Mar-01.
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
 // Change the following variables to customize the appearance of the particles
 var numParticles = window.innerWidth / 3;
-var maxSpeed = 2.5;
-var minSpeed = 1;
-var maxSize = 5;
-var minSize = 3;
-var maxOpacity = 1;
-var minOpacity = 0.2;
+var maxSpeed = 0.7;
+var minSpeed = 0.1;
+var maxSize = 2.5;
+var minSize = 0.5;
+var maxOpacity = 0.7;
+var minOpacity = 0.1;
+var horizontalMovementFrequency = 42; // Lower = More frequent
+var horizontalMovementSize = 1.5;
 var canvasId = "snowcanvas";
 
 // If the following is true, the canvas resolution will be set to match the full viewport width and height.
@@ -19,15 +22,12 @@ var fixCanvasResolution = true;
 var snow_particles = [];
 
 window.onload = function() {
-
 	if (fixCanvasResolution) {
 		var c = document.getElementById(canvasId);
 		c.width = window.innerWidth;
 		c.height = window.innerHeight;
 	}
-
 	InitPoints();
-
 	setInterval(Redraw, 5);
 }
 
@@ -45,6 +45,18 @@ function Redraw() {
 			snow_particles[i].size = Math.random() * (maxSize - minSize) + minSize;
 		}
 		snow_particles[i].yPos = newYPos;
+
+		// Occasionally move snow particle horizontally
+		if (Math.floor(Math.random() * (horizontalMovementFrequency / snow_particles[i].speed)) == 0) {
+			var newXPos = snow_particles[i].xPos + (Math.random() * horizontalMovementSize * 2 - horizontalMovementSize);
+			if (newXPos < 0) {
+				newXPos = window.innerWidth - 1; // Wrap disappearing particle to right side of screen
+			} else if (newXPos >= window.innerWidth) {
+				newXPos = 0; // Wrap disappearing particle to left side of screen
+			}
+			snow_particles[i].xPos = newXPos;
+		}
+
 		ctx.beginPath();
 		ctx.arc(snow_particles[i].xPos, newYPos, snow_particles[i].size, 0, 2 * Math.PI);
 		ctx.fillStyle = "rgba(255, 255, 255, " + snow_particles[i].opacity + ")";
